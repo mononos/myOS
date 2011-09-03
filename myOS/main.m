6d9a178f64f40d86a028b6292180d77124686213
mononos <nos.nbs@gmail.com>
2011-09-03 11:20:58 +0700

    Initial Commit

---
+++ myOS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  myOS
+//
+//  Created by Bodin Milindankura on 9/3/54 BE.
+//  Copyright 2554 nos.nbs@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
---
+++ myOS/myOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  myOSAppDelegate.h
+//  myOS
+//
+//  Created by Bodin Milindankura on 9/3/54 BE.
+//  Copyright 2554 nos.nbs@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myOSAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
